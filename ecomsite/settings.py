a27a5563e52e7f8c1f713e1aa2cac6341cbef7c8
Ridhesh Chauhan <123955317+Ridhesh05@users.noreply.github.com>
2024-02-26 17:58:46 +0530

    Update settings.py

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -4,6 +4,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "ridhesh.pythonanywhere.com"]
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
